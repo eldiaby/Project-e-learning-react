633e335009867950d3fae2893e353a0ce7c8b7df
unknown <elgdiab@gmail.com>
2024-08-24 22:00:35 +0300

    uplouding the footer section

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import '@fortawesome/fontawesome-free/css/all.min.css'; // Import Font Awesome CSS
+import './Footer.css'; // Import custom CSS for additional styling
 
-export default function Footer() {
-  return <>
-     <h2>footer</h2>
-  </>
-}
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark text-white">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 text-center text-md-left">
+                        <p className="mb-0">&copy; 2024 Your Company. All rights reserved.</p>
+                    </div>
+                    <div className="col-md-6 text-center text-md-right">
+                        <div className="social-icons">
+                            <a href="#"  className="mx-2">
+                                <i className="fab fa-facebook-f"></i>
+                            </a>
+                            <a href="#" className="mx-2">
+                                <i className="fab fa-twitter"></i>
+                            </a>
+                            <a href="#" className="mx-2">
+                                <i className="fab fa-linkedin-in"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
