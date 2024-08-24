642fc36307f0ebb95ffb35d5b0c0d490bfa7f292
unknown <elgdiab@gmail.com>
2024-08-24 22:09:06 +0300

    updating the footer section

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import './Footer.css';
+import './footer.css';
 
 const Footer = () => {
     return (
